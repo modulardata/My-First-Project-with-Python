599ff9291485a6ebd417a116758f9f2196ba330b
Miodrag <3128205+modulardata@users.noreply.github.com>
2024-05-02 22:49:52 +0200

    Stage 3/3

--- My First Project with Python/task/calculator.py
+++ My First Project with Python/task/calculator.py
@@ -11,3 +11,8 @@ for key, value in products.items():
     print(f'{key}: ${value}')
 print()
 print(f'Income: ${total_income}')
+stuff_expenses = input('Staff expenses:\n')
+other_expenses = input('Other expenses:\n')
+total_expenses = int(stuff_expenses) + int(other_expenses)
+
+print(f'Net income: ${total_income - total_expenses}')
